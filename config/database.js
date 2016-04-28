56231f4cd02f366e4f4d22b4a2f3442b6890297b
rohit0212 <kumar.rohit0212@gmail.com>
2016-04-28 14:03:18 -0700

    database url changes

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : "mongodb://localhost:27017/get2leads"// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' :     "mongodb://get2leads:get2leads@ds021771.mlab.com:21771/get2leads"      //"mongodb://localhost:27017/get2leads"// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
